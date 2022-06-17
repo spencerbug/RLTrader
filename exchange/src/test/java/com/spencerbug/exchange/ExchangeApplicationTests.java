9930c4164aa7cc7cf7c51d2da5883cc1094d40ed
Spencer Neilan <spencer.neilan@gmail.com>
2022-06-17 09:42:05 -0500

    Decided to use the matching engine provided by Exchange-Core Started adding a spring-boot application as an interface to the exchange Closing #1, as the exchange-core counts as a monolithic block for the matching engine, and multicast can be considered in part of the exchange ticket

---
+++ exchange/src/test/java/com/spencerbug/exchange/ExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spencerbug.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
